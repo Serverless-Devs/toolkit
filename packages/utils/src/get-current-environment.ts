26c90d26cadbfcaa48a9283d49213c7ee325c154
xsahxl <xsahxl@126.com>
2023-03-21 16:47:49 +0800

    feat: getCurrentEnvironment 识别 serverless_cd 环境
    Signed-off-by: xsahxl <xsahxl@126.com>

--- packages/utils/src/get-current-environment.ts
+++ packages/utils/src/get-current-environment.ts
@@ -2,7 +2,9 @@ const getCurrentEnvironment = () => {
   if (process.env.HOME === '/kaniko' && process.env.BUILD_IMAGE_ENV === 'fc-backend') {
     return 'app_center';
   }
+
   for (const key in process.env) {
+    if (key === 'SERVERLESS_CD') return 'serverless_cd';
     if (key.startsWith('CLOUDSHELL')) return 'cloud_shell';
     if (key.startsWith('PIPELINE')) return 'yunxiao';
     if (key.startsWith('GITHUB')) return 'github';
