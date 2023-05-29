c5251033ae0da3b90b1b9c0101f0059aaf10ab5c
wss <wssgryx@163.com>
2023-05-29 18:29:24 +0800

    feat: 🎉 add ignore-walk
    Signed-off-by: wss <wssgryx@163.com>

--- packages/ignore-walk/__tests__/index.test.ts
+++ packages/ignore-walk/__tests__/index.test.ts
@@ -1,6 +1,46 @@
-import Zip from '../src';
+import ignoreWalk from '../src';
 import path from 'path';
+import { Minimatch, minimatch } from 'minimatch';
 
-test('download url is empty', async () => {
+const codePath = path.join(__dirname, 'fixtures', 'ignore-code');
+
+test('ignore walk', async () => {
+  const zipFiles = await ignoreWalk({
+    ignoreFiles: ['.fcignore'],
+    path: codePath,
+    includeEmpty: true,
+  });
+
+  console.log('zipFiles: ', zipFiles, (zipFiles as any).includes('keep-empty-dir'))
+  
+  expect(zipFiles).toEqual(expect.arrayContaining([
+    '.fcignore',
+    'apt-get.list',
+    'dir-2/.hide-dir/apt-get.list',
+    'dir-2/no-ignore/file',
+    'empty-dir',
+    'ignore-non-root-dir/index',
+    'index.js',
+    // 'keep-empty-dir'
+  ]));
+
   // await expect().rejects.toThrow('url is required');
 });
+
+test.only('ignore walk', async () => {
+  const zipFiles = await ignoreWalk({
+    ignoreFiles: ['.fcignore'],
+    path: path.join(__dirname, 'fixtures', 'ignore-2'),
+    includeEmpty: true,
+  });
+
+  console.log('zipFiles: ', zipFiles, (zipFiles as any).includes('keep-empty-dir'))
+});
+
+test('xx', () => {
+  const m = new Minimatch('keep-empty-dir/**', { matchBase: true, dot: true, flipNegate: true, nocase: true });
+  console.log(m.match('keep-empty-dir/ignore-file'));
+  console.log(m.match('keep-empty-dir/'));
+  console.log(m.match('/keep-empty-dir'));
+  console.log(m.match('keep-empty-dir'));
+})
--- packages/ignore-walk/src/types.ts
+++ packages/ignore-walk/src/types.ts
@@ -1,9 +1,32 @@
 
 export interface IOptions {
    /**
-   * Path to where your file will be written.
+   * root dir to start in.
    *
    * @default process.cwd()
    */
-   dest?: string;
+   path?: string;
+   /**
+   * list of filenames.
+   *
+   * @default ['.signore']
+   */
+   ignoreFiles?: string[];
+   /**
+   * true to include empty dirs
+   *
+   * @default false
+   */
+   includeEmpty?: boolean;
+   /**
+   * true to follow symlink dirs
+   *
+   * @default false
+   */
+   follow?: boolean;
+   /**
+   * set to true if this.path is a symlink, whether follow is true or not
+   *
+   */
+   isSymbolicLink?: boolean;
 }
