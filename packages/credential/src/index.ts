0433e892c93a9079b69d5b5b307cae17b87f84d5
wss <wssgryx@163.com>
2023-06-12 11:13:21 +0800

    feat: 🎉 初始Credential
    Signed-off-by: wss <wssgryx@163.com>

---
+++ packages/credential/src/index.ts
@@ -0,0 +1,18 @@
+
+export default class Credential {
+  static async get() {
+
+  }
+
+  static async set() {
+    
+  }
+
+  static async remove() {
+    
+  }
+
+  static async decrypt() {
+    
+  }
+}
