68ffc327499a399d898dc557d57e7ed15285bf8c
wss-git <wssgryx@163.com>
2023-07-11 11:13:30 +0800

    style: 🎨 format
    Signed-off-by: wss-git <wssgryx@163.com>

--- packages/component-interface/src/index.ts
+++ packages/component-interface/src/index.ts
@@ -11,8 +11,8 @@ export interface InputProps {
   yaml: {
     path: string; // 配置路径
   };
-  resource:{
-    name: string, // yaml 声明 resource 的 name
+  resource: {
+    name: string; // yaml 声明 resource 的 name
     component: string; // 组件名（支持本地绝对路径）
     access: string; // 访问秘钥名
   };
